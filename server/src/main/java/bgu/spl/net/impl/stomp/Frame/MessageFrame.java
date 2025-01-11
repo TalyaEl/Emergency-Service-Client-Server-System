7fd5d9e2946b50830e2b571c4e13515f9262cd32
Talya Eliyahu <talyaeli@post.bgu.ac.il>
2025-01-11 16:16:27 +0000

    cont connections - implementation of the 3 functions

--- server/src/main/java/bgu/spl/net/impl/stomp/Frame/MessageFrame.java
+++ server/src/main/java/bgu/spl/net/impl/stomp/Frame/MessageFrame.java
@@ -1,11 +1,12 @@
 package bgu.spl.net.impl.stomp.Frame;
 
 public class MessageFrame extends StompFrameAbstract {
-    public MessageFrame(int clientID, int serverId, String dest, String inBody){
+    public MessageFrame(int clientID, int msgId, String dest, String inBody){
         super("MESSAGE");
         this.headers.put("subscription:", String.valueOf(clientID));
-        this.headers.put("message-id:", String.valueOf(serverId));
+        this.headers.put("message-id:", String.valueOf(msgId));
         this.headers.put("destination:", dest);
         this.body = inBody;
     }
+    
 }
--- server/src/main/java/bgu/spl/net/srv/ConnectionsImpl.java
+++ server/src/main/java/bgu/spl/net/srv/ConnectionsImpl.java
@@ -3,17 +3,23 @@ import java.io.IOException;
 import java.util.LinkedList;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
-
+import java.util.concurrent.atomic.AtomicInteger;
 import bgu.spl.net.impl.stomp.Frame.ErrorFrame;
+import bgu.spl.net.impl.stomp.Frame.MessageFrame;
 import bgu.spl.net.srv.NonBlockingConnectionHandler;
+
 public class ConnectionsImpl<T> implements Connections<T> {
-    
+    // private static final AtomicInteger counter = new AtomicInteger(1);
+    // private final int msgId;
     private ConcurrentHashMap<Integer, NonBlockingConnectionHandler<T>> handlers;
+    private ConcurrentHashMap<Integer, Set<String>> clientSubscriptions;
     private ConcurrentHashMap<String, Set<Integer>> channelSubscribers;
 
     public ConnectionsImpl() {
+        // this.msgId = counter.getAndIncrement();
         this.handlers = new ConcurrentHashMap<>();
         this.channelSubscribers = new ConcurrentHashMap<>();
+        this.clientSubscriptions = new ConcurrentHashMap<>();
     }
 
     @Override
@@ -30,13 +36,29 @@ public class ConnectionsImpl<T> implements Connections<T> {
     public void send(String channel, T msg) {
         Set<Integer> subscribers = channelSubscribers.get(channel);
         if (subscribers != null) {
-                for (Integer sub : subscribers) {
-                    NonBlockingConnectionHandler<T> handler = handlers.get(sub);
-                    if (!send(sub, msg)) {
-                        ErrorFrame error = new ErrorFrame(channel, 0, channel, null)
-                        handler.close();
-                    }
+            for (Integer sub : subscribers) {
+                // NonBlockingConnectionHandler<T> handler = handlers.get(sub);
+                // MessageFrame frame = new MessageFrame(sub, msgId, channel, msg.toString());
+                if (!send(sub, msg)) {
+                    System.out.println("couldn't send, connection terminated for" + sub);
+                    // ErrorFrame error = new ErrorFrame("couldn't send", sub, null, frame);
+                    disconnect(sub);
                 }
+            }
+        }
+    }
+
+    @Override
+    public void disconnect(int connectionId) {
+        if (handlers.containsKey(connectionId)) {
+            NonBlockingConnectionHandler<T> handler = handlers.get(connectionId);
+            handler.close();
+            Set<String> set = clientSubscriptions.get(connectionId);
+            for (String channel : set) { //for each channel, remove this client from it's subscribers
+                channelSubscribers.get(channel).remove(connectionId);
+            }
+            handlers.remove(connectionId);
+            clientSubscriptions.remove(connectionId);
         }
     }
 }
