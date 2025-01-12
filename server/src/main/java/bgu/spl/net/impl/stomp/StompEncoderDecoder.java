ffec5bd7934b98de1ca9599978dc508e8d9a98d2
Talya Eliyahu <talyaeli@post.bgu.ac.il>
2025-01-12 19:10:15 +0000

    change encdec to stomp abstract, decode not completed

--- server/src/main/java/bgu/spl/net/impl/stomp/StompEncoderDecoder.java
+++ server/src/main/java/bgu/spl/net/impl/stomp/StompEncoderDecoder.java
@@ -1,18 +1,41 @@
 package bgu.spl.net.impl.stomp;
-
-import java.io.Serializable;
-import java.nio.ByteBuffer;
-
 import bgu.spl.net.api.MessageEncoderDecoder;
+import bgu.spl.net.impl.stomp.Frame.StompFrameAbstract;
 
-public class StompEncoderDecoder implements MessageEncoderDecoder<Serializable> {
-    // private final ByteBuffer buf;
+public class StompEncoderDecoder implements MessageEncoderDecoder<StompFrameAbstract> {
+    private byte[] bytes = new byte[1 << 10];
+    private int len = 0;
+    private StompFrameAbstract currentFrame = null;
 
-    public Serializable decodeNextByte(byte nextByte) {
+    @Override
+    public StompFrameAbstract decodeNextByte(byte nextByte) {
+        if (nextByte == '\0') { //if we're at the end of the frame
+            if (currentFrame != null) { //and also if the frame we created isn't null
+                StompFrameAbstract ansFrame = currentFrame;
+                len = 0;
+                currentFrame = null;
+                return ansFrame;
+            }
+        }
+        else {
+            pushByte(nextByte);
+        }
         return null;
     }
 
-    public byte[] encode(Serializable message) {
+    private void pushByte(byte nextByte) {
+        if (len >= bytes.length) {
+            byte[] newBytes = new byte[bytes.length * 2];
+            for (int i = 0; i <bytes.length; i++) {
+                newBytes[i] = bytes[i];
+            }
+            bytes = newBytes;
+        }
+        bytes[len++] = nextByte;
+    }
+
+    @Override
+    public byte[] encode(StompFrameAbstract message) {
         return null;
     }
 }
