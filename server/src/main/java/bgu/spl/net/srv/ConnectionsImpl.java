6373824ff1ce5a59764b3b36db80cd9eb76f4514
Talya Eliyahu <talyaeli@post.bgu.ac.il>
2025-01-16 10:02:10 +0000

    changing connecionsImpl to connection handler instead nonBlocking

--- server/src/main/java/bgu/spl/net/srv/ConnectionsImpl.java
+++ server/src/main/java/bgu/spl/net/srv/ConnectionsImpl.java
@@ -8,7 +8,7 @@ import bgu.spl.net.srv.NonBlockingConnectionHandler;
 
 public class ConnectionsImpl<T> implements Connections<T> {
     private final AtomicInteger clientId = new AtomicInteger(0);
-    private ConcurrentHashMap<Integer, NonBlockingConnectionHandler<T>> handlers;
+    private ConcurrentHashMap<Integer, ConnectionHandler<T>> handlers;
     private ConcurrentHashMap<String, ConcurrentHashMap<Integer, Boolean>> channelSubscribers;
 
     public ConnectionsImpl() {
@@ -16,7 +16,7 @@ public class ConnectionsImpl<T> implements Connections<T> {
         this.channelSubscribers = new ConcurrentHashMap<>();
     }
 
-   public synchronized void addConnection(NonBlockingConnectionHandler<T> handler) {
+   public synchronized void addConnection(ConnectionHandler<T> handler) {
         if (handler != null) {
             int connectionId = clientId.incrementAndGet();
             handlers.put(connectionId, handler);
@@ -30,11 +30,7 @@ public class ConnectionsImpl<T> implements Connections<T> {
     @Override
     public boolean send(int connectionId, T msg) {
         if (handlers.containsKey(connectionId)) {
-            NonBlockingConnectionHandler<T> handler = handlers.get(connectionId);
-            if (handler.isClosed()) {
-                disconnect(connectionId);
-                System.out.println("couldn't send, connection terminated for" + connectionId);
-            }
+            ConnectionHandler<T> handler = handlers.get(connectionId);
             try {
                 handler.send(msg);
             } catch (IllegalStateException e) {
@@ -58,13 +54,15 @@ public class ConnectionsImpl<T> implements Connections<T> {
     @Override
     public synchronized void disconnect(int connectionId) {
         if (handlers.containsKey(connectionId)) {
-            NonBlockingConnectionHandler<T> handler = handlers.get(connectionId);
-            handler.close();
-            for (String channel : channelSubscribers.keySet()) {
-                ConcurrentHashMap<Integer, Boolean> subscribers = channelSubscribers.get(channel);
-                if (subscribers != null) {
-                    subscribers.remove(connectionId);
-                }
+            ConnectionHandler<T> handler = handlers.get(connectionId);
+            try {
+                handler.close();
+            } catch (IOException e) {}
+        }
+        for (String channel : channelSubscribers.keySet()) {
+            ConcurrentHashMap<Integer, Boolean> subscribers = channelSubscribers.get(channel);
+            if (subscribers != null) {
+                subscribers.remove(connectionId);
             }
         }
     }
