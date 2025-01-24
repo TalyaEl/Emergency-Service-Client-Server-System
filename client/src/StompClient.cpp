e2d49cf396c88e69c0e07f3deb718af0cc12d19d
Talya Eliyahu <talyaeli@post.bgu.ac.il>
2025-01-24 15:10:49 +0000

    creating stompFrame for client's side and starting implementing the client (adding a header for stomp client)

--- client/src/StompClient.cpp
+++ client/src/StompClient.cpp
@@ -1,3 +1,45 @@
+#include "StompClient.h"
+#include "ConnectionHandler.h"
+#include "StompProtocol.h"
+#include <iostream>
+#include <sstream>
+
+using std::make_unique;
+using std::thread;
+
+StompClient::StompClient() : connection(make_unique<ConnectionHandler>()), protocol(make_unique<StompProtocol>()) {}
+
+StompClient::~StompClient() {
+	stop();
+}
+
+void StompClient::connect (const string& host, short port) {
+	try { 
+		//initialize connection
+		connection = make_unique<ConnectionHandler>(host, port);
+
+		if (!connection -> connect()) { //if the handler couldn't connect to the server
+			throw std::runtime_error("failed to connect to server");
+		}
+
+		socket_thread = thread()
+	}
+}
+
+
+void StompClient::stop() {
+    is_running = false;  //let the threads know to stop running
+    
+    if (socket_thread.joinable()) { //check if the socket thread is active
+        socket_thread.join();  //if so, wait for it to finish the work
+    }
+    
+    if (connection) { //checks that it's not null pointer
+        connection->close();  //close network connection if exist
+    }
+}
+
+
 int main(int argc, char *argv[]) {
 	// TODO: implement the STOMP client
 	return 0;
--- client/src/StompFrame.cpp
+++ client/src/StompFrame.cpp
@@ -1,6 +1,7 @@
 #include "../include/StompFrame.h"
 #include <sstream>
 using std::istringstream;
+using std::stringstream;
 
 //constructor
 StompFrame::StompFrame(string cmd, map<string, string> hdrs, string frameBody) :
@@ -21,18 +22,38 @@ StompFrame StompFrame::parse(const string& stringFrame) {
     while (getline(stream, line) && !line.empty()) {
         size_t colonIndex = line.find(":");
         if (colonIndex != string::npos) { //if colon is found
-            headers[line.substr(0, colonIndex)] = line.substr(colonIndex + 1);
+            string key = line.substr(0, colonIndex);
+            string value = line.substr(colonIndex + 1);
+            value.erase(0, value.find_first_not_of(" "));
+            headers[key] = value;
         }
     }
-
+    //get the body
     string bodyLine;
     while (getline(stream, bodyLine)) { //trim new line from body
-        body += bodyLine + "\n";
+        if (bodyLine[0] == '\0') break;
+        body += bodyLine;
     }
 
     return StompFrame(command, headers, body);
 }
 
- string StompFrame::serialize() const {
-    
- }
+string StompFrame::serialize() const {
+    stringstream ss;
+    ss << command << '/n';
+
+    for (const auto& header : headers) {
+        ss << header.first << ":" << header.second << "\n";
+    }
+
+    ss << "\n"; //add empty line between headers and body
+    ss << body;
+    ss << "\0";
+
+    return ss.str();
+}
+
+//getters
+string StompFrame::getCommand() const {return command;}
+map<string, string> StompFrame::getHeaders() const {return headers;}
+string StompFrame::getBody() const {return body;}
