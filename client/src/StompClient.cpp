86e3db5a1953359ad2226b9f9b41626c89395213
Talya Eliyahu <talyaeli@post.bgu.ac.il>
2025-01-25 20:03:04 +0000

    jsons for checking and deleting (connection) in client

--- client/src/StompClient.cpp
+++ client/src/StompClient.cpp
@@ -90,7 +90,7 @@ void StompClient::process_keyboard_input() {
 			if (args[0] == "login") {
                 StompFrame frame = protocol -> processKeyboardInput(args); //getting the connected frame
                 if (frame.getCommand() != "") {
-                    cout << frame.getCommand() << "\n";
+                    cout << frame.getCommand() << "\n"; //testinggggggg
                     handleLogin(args);
                     connection->sendFrameAscii(frame.serialize(), '\0');
                 }            	
@@ -99,6 +99,7 @@ void StompClient::process_keyboard_input() {
 				vector<StompFrame> eventsReported = protocol -> report(args[1]);
 				if (!eventsReported.empty()) {
 					for (StompFrame frame : eventsReported) {
+                        cout << frame.getCommand() << "\n"; //TESTTTTTT
 						connection->sendFrameAscii(frame.serialize(), '\0');
 					}
 				}
@@ -108,11 +109,21 @@ void StompClient::process_keyboard_input() {
 				protocol -> processKeyboardInput(args);
 			}
 			//checks that it's not null pointer
-			else if (connection) { //other cases rather than login and report and summary
-				StompFrame frame = protocol -> processKeyboardInput(args);
+			// else if (connection) { //other cases rather than login and report and summary
+            else {
+                StompFrame frame = protocol -> processKeyboardInput(args);
                 if (frame.getCommand() != "") {
+                    cout << frame.getCommand() << "\n"; //testinggggggg
+
                 	connection->sendFrameAscii(frame.serialize(), '\0');
                 }
+            // }
+				// StompFrame frame = protocol -> processKeyboardInput(args);
+                // if (frame.getCommand() != "") {
+                //     cout << frame.getCommand() << "\n"; //testinggggggg
+
+                // 	connection->sendFrameAscii(frame.serialize(), '\0');
+                // }
 			}
         }
         catch (const exception& e) {
